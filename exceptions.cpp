2b4ea34645ba9409576a2712177d03e0c13adcdc
VenKrish3 <kv72810@mail.harpercollege.edu>
2024-10-16 16:39:57 -0500

    Update exceptions.cpp

--- exceptions.cpp
+++ exceptions.cpp
@@ -1,12 +1,16 @@
 #include <iostream>
+
 using namespace std;
-class Excp1 : public exception {
+
+class Excp1 : public exception 
+{
     virtual const char *what() const throw()
     {
         return "argumentoneisnogood";
     }
     
 } e1;
+
 class Excp2 : public exception {
     virtual const char *what() const throw()
     {
@@ -14,6 +18,7 @@ class Excp2 : public exception {
     }
     
 } e2;
+
 class Excp3 : public exception {
     virtual const char *what() const throw()
     {
@@ -22,11 +27,42 @@ class Excp3 : public exception {
     
 } e3;
 
-void my_exceptions(int &num1, int &num2, int &num3) {
-    
+void my_exceptions(int num1, int num2, int num3) 
+{	
+    try
+	{
+    	if(num1 > 0)
+    	{
+    		throw e1;	
+		}	
+		if(num2 == num1)
+		{
+			throw e2;
+		}	
+		if(num1 + num2 == num3)
+		{
+			throw e3;
+		}
+		cout << num1 + num2 + num3 << endl;
+	}
+	catch(exception &ex)
+	{
+		cout << ex.what() << endl;
+	}
 }
-int main() {
 
-
+int main() 
+{
+	int num1, num2, num3;
+	
+	cout << "enter first value" << endl;
+	cin >> num1;
+	cout << "enter second value" << endl;
+	cin >> num2;
+	cout << "enter third value" << endl;
+	cin >> num3;
+	
+	my_exceptions(num1,num2,num3);
+		
     return 0;
 }
